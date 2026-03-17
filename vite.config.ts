77021dde88cbf00522de6a7049910e42a57e4d03
yezi128517 <yezi128517@gmail.com>
2026-03-17 11:00:05 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/memoa/',
+  base: './', // 改为相对路径，最稳妥的兼容方案
   build: {
     outDir: 'dist',
   }
