a62a52c7d407e28ecc891609c721708ed772bfd5
yezi128517 <yezi128517@gmail.com>
2026-03-17 11:04:23 +0800

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,10 +1,24 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// 确保这里的 ID 'root' 必须和 index.html 里的 <div id="root"></div> 完全一致
+const container = document.getElementById('root');
+
+if (!container) {
+  throw new Error("未找到根节点 'root'，请检查 index.html 中是否存在 <div id='root'></div>");
+}
+
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 );
