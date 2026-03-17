fb07d28b320c142fe67ec97a80ab996830330da7
yezi128517 <yezi128517@gmail.com>
2026-03-17 10:55:44 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // 关键修复点：将 base 设置为你的新仓库名
-  // 这样浏览器就会去 https://memoa.github.io/memoa/ 下寻找资源
-  base: '/memoa/',
   plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-  },
+  // 必须加上这一行，且名字必须是 memoa
+  base: '/memoa/',
+})
 })
