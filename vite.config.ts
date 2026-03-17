ea3d9878b2514c48341c17ab369c86c1cd1eed64
yezi128517 <yezi128517@gmail.com>
2026-03-17 10:57:09 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 必须加上这一行，且名字必须是 memoa
   base: '/memoa/',
-})
+  build: {
+    outDir: 'dist',
+  }
 })
